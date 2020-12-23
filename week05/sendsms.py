e9f82c9e061055e1db7bdb80a2a90a65d6bc8e6c
leolongpy <leolongpy@qq.com>
2020-12-23 09:04:08 +0800

    week5

--- week05/sendsms.py
+++ week05/sendsms.py
@@ -2,12 +2,12 @@ import redis
 
 r = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
 def sendsms(telephone_number, content):
-    num = r.hget('telnum',telephone_number)
+    num = r.get(f'telnum_{telephone_number}')
     if num and int(num) > 5:
         print('发送失败，请一分钟后重试')
     else:
         print(content)
-        r.hincrby('telnum',telephone_number, amount=1)
+        r.incrby(f'telnum_{telephone_number}', amount=1)
         if not num:
             r.expire('telnum',60)
 
